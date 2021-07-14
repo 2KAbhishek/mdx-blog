523c0b43540fcf49d167d71bc9745e68c9d18038
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 03:14:22 +0530

    Add theme-ui colors

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+        text: '#111',
+        background: '#eee',
+        primary: '#097AFF',
+        secondary: '#FF8A00',
+        accent: '#1688F0',
+        highlight: '#1688F0',
+        muted: '#F2E0D7'
+    }
+};
+export default theme;
