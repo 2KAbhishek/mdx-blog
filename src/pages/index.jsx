97d58d18437bd552386ba4f471abe470c763b708
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-14 20:22:36 +0530

    Use layout in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import {Link} from 'gatsby';
-
-// styles
-const pageStyles = {
-    color: '#232129',
-    padding: 96,
-    fontFamily: '-apple-system, Roboto, sans-serif, serif'
-};
-const headingStyles = {
-    marginTop: 0,
-    marginBottom: 24,
-    maxWidth: 320
-};
-
+import Layout from '../components/layout';
 // markup
 const IndexPage = () => {
     return (
-        <main style={pageStyles}>
-            <title>MDX Blog</title>
-            <h1 style={headingStyles}>MDX Blog</h1>
+        <Layout pageTitle='MDX Blog'>
             <p>Welcome to MDX Blog</p>
-            <Link to='/about'>About MDX Blog</Link>
-        </main>
+        </Layout>
     );
 };
 
