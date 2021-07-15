9d9374d42a9b8f3754ff4c9196f77fcd89d6a13f
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 16:14:11 +0530

    Use graphQL to pull data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Layout from '../components/layout';
+import {useStaticQuery, graphql} from 'gatsby';
 import {StaticImage} from 'gatsby-plugin-image';
 
 const IndexPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `);
+    const title = data.site.siteMetadata.title;
     return (
-        <Layout pageTitle='MDX Blog'>
-            <p>Welcome to MDX Blog</p>
+        <Layout pageTitle={title}>
+            <p>Welcome to {title}</p>
             <StaticImage
                 src='../images/icon.png'
                 alt='Markdown logo'></StaticImage>
