be8842cef7677a3716ef79f484c92ae14470eee4
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 23:45:31 +0530

    Add blog post template

---
+++ src/templates/blog-post.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default function BlogPost() {
+    return (
+        <Layout pageTitle='Blog Post'>
+            <div>Blog Post Template</div>
+        </Layout>
+    );
+}
