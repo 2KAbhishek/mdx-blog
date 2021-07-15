1f369554f99617fd266b04a0c6e65eba4d4d60f5
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 19:10:16 +0530

    Update font style

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import {graphql} from 'gatsby';
 import Layout from '../components/layout';
 
-const Articles = () => {
+const Articles = ({data}) => {
+    const articles = data.allFile.nodes.filter((node) => node.ext === '.mdx');
     return (
         <Layout pageTitle='My Articles'>
             <p>Here are my articles</p>
+            <ul>
+                {articles.map((node) => (
+                    <li key={node.name}>{node.name}</li>
+                ))}
+            </ul>
         </Layout>
     );
 };
+
+export const query = graphql`
+    query {
+        allFile {
+            nodes {
+                name
+                ext
+            }
+        }
+    }
+`;
 
 export default Articles;
