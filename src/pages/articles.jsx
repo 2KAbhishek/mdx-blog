9cc38ac744776395d516b64db6255f11665e837e
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 16:19:09 +0530

    Add articles page

---
+++ src/pages/articles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Articles = () => {
+    return (
+        <Layout pageTitle='My Articles'>
+            <p>Here are my articles</p>
+        </Layout>
+    );
+};
+
+export default Articles;
