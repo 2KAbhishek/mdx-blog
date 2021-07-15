87c75598e1738e62971b7521d7786428a555b72b
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 19:11:02 +0530

    Use style module

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {graphql} from 'gatsby';
 import Layout from '../components/layout';
+import {blogList, blogItem} from './articles.module.css';
 
 const Articles = ({data}) => {
     const articles = data.allFile.nodes.filter((node) => node.ext === '.mdx');
     return (
         <Layout pageTitle='My Articles'>
-            <p>Here are my articles</p>
-            <ul>
+            <ul className={blogList}>
                 {articles.map((node) => (
-                    <li key={node.name}>{node.name}</li>
+                    <li className={blogItem} key={node.name}>
+                        <h3>{node.name}</h3>
+                    </li>
                 ))}
             </ul>
         </Layout>
