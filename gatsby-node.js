ed33bcb1b1bf540695f5c525d8008747e7ff21cf
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 23:59:33 +0530

    Update page path

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ exports.createPages = async ({graphql, actions}) => {
     `);
     result.data.allMdx.edges.forEach(({node}) => {
         createPage({
-            path: node.slug,
+            path: '/articles/' + node.slug,
             component: path.resolve(`./src/templates/blog-post.js`),
             context: {
                 slug: node.slug
