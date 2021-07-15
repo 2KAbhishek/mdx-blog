0f4d41ef82098ae9bf4c14ff519b10c03a38fea8
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 19:31:43 +0530

    Update colors

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,10 +2,10 @@ const theme = {
     colors: {
         text: '#111',
         background: '#eee',
-        primary: '#097AFF',
-        secondary: '#FF8A00',
+        primary: '#1688F0',
+        secondary: '#D17000',
         accent: '#1688F0',
-        highlight: '#1688F0',
+        highlight: '#097AFF',
         muted: '#F2E0D7'
     }
 };
