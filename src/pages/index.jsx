e28c1391ac71e3b0919eec7f321373e870f0a6b7
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-15 10:48:22 +0530

    Update icon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
+import {StaticImage} from 'gatsby-plugin-image';
 
 const IndexPage = () => {
     return (
         <Layout pageTitle='MDX Blog'>
             <p>Welcome to MDX Blog</p>
+            <StaticImage
+                src='../images/icon.png'
+                alt='Markdown logo'></StaticImage>
         </Layout>
     );
 };
