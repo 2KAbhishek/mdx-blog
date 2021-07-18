6424c42215cf36381fb5b14c203e485f4364fc5a
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-19 00:41:47 +0530

    Update theme ui colors

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -6,7 +6,7 @@ const theme = {
         secondary: '#D17000',
         accent: '#1688F0',
         highlight: '#097AFF',
-        muted: '#F2E0D7'
+        muted: '#7B7B7B'
     }
 };
 export default theme;
