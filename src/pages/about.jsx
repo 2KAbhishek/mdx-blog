bee76adc57a780d1176c758f4a481317515c455d
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-19 00:42:31 +0530

    Add link to source

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,11 +6,18 @@ const AboutPage = () => {
         <Layout pageTitle='About'>
             <p>
                 This blog was created by
-                <a href='https://www.github.com/2kabhsihek'> 2KAbhishek </a>.
+                <a href='https://www.github.com/2kabhishek'> 2KAbhishek </a>.
                 <br />
                 <br />
                 Made using Gatsby JS, React, GraphQL, Theme UI and a bazillion
                 more things
+                <br />
+                <br />
+                View the source here{' '}
+                <a href='https://github.com/2kabhishek/mdx-blog'>
+                    MDX Blog on GitHub
+                </a>
+                .
             </p>
         </Layout>
     );
