30c3f0565ca4a4ed87626d005d3b05908d2517d2
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-18 21:00:55 +0530

    Add blog-post style module

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import {graphql} from 'gatsby';
 import {MDXRenderer} from 'gatsby-plugin-mdx';
+import {} from './blog-post.modules.css';
 
 export default function BlogPost({data}) {
     return (
